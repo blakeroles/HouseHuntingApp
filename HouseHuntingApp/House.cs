f95312cacacba6db857060f9c4baabcd8109f8d2
blake_roles <blakeroles@gmail.com>
2020-02-07 22:32:12 +1100

    Added house class

---
+++ HouseHuntingApp/House.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseHuntingApp
+{
+    class House
+    {
+        // Attributes
+        private string type;
+        private double price;
+        private string location;
+        private int postcode;
+        private int bedrooms;
+        private int bathrooms;
+        private int carSpaces;
+
+        // Constructor
+        public House()
+        {
+            type = "unknown";
+            price = 99999999.99;
+            location = "unknown";
+            postcode = 9999;
+            bedrooms = 99;
+            bathrooms = 99;
+            carSpaces = 99;
+        }
+    }
+}
